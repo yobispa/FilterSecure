e22fd8efbd381fe91e287a280bcfdb8126953991
Yoandel Obispa <302631801@student.rocmondriaan.nl>
2023-04-03 23:38:51 +0200

    added navbar, put the bootstrap cards center and added a nice transition by the navbar

--- js/main.js
+++ js/main.js
@@ -1,5 +1,1 @@
-fetch("https://nl.aliexpress.com/item/1005004998757903.html")                             
-.then (res => res.json())
-.then (data => {
-    console.log(data)
-})
+console.log("JS Loaded")
