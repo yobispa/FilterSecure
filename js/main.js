04b1adeafa0be1bab73bca3007552be87d8f7a76
Yoandel Obispa <302631801@student.rocmondriaan.nl>
2023-04-06 01:27:09 +0200

    added navbar autohide, added home page, added phtotos to cards, added cards

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
 console.log("JS Loaded")
+document.addEventListener("DOMContentLoaded", function(){
+    const el_autohide = document.querySelector('.autohide');
+    if(el_autohide){
+        
+        let last_scroll_top = 0;
+        window.addEventListener('scroll', function() {
+               let scroll_top = window.scrollY;
+           if(scroll_top < last_scroll_top) {
+                el_autohide.classList.remove('scrolled-down');
+                el_autohide.classList.add('scrolled-up');
+            }
+            else {
+                el_autohide.classList.remove('scrolled-up');
+                el_autohide.classList.add('scrolled-down');
+            }
+            last_scroll_top = scroll_top;
+  
+        }); 
+        // window.addEventListener
+  
+    }
+    
+  }); // // DOMContentLoaded  end
