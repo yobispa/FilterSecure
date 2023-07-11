c8800f7d88ff1a37919af3fa58be68e14a5d8bd0
Yoandel Obispa <302631801@student.rocmondriaan.nl>
2023-07-11 05:46:01 +0200

    added counter for items

--- js/cart.js
+++ js/cart.js
@@ -23,13 +23,20 @@ function createCard(jsonData) {
   }
     for(let i = 0; i < retArray.length; i++) {
       document.querySelector(".total-price").innerHTML = ``
-        const card = `<div class="card w-auto h-auto">
-          <img class="card-img-top w-50 h-50" src="${jsonData.products[retArray[i]].img1}" alt="Filter image">
+        const card = `<div class="card w-50 h-auto">
+          <img class="card-img-top w-100 h-50 border-bottom" src="${jsonData.products[retArray[i]].img1}" alt="Filter image">
           <div class="card-body">
             <h4 class="card-title">${jsonData.products[retArray[i]].modelnumber}</h4>
             <p class="card-text">${jsonData.products[retArray[i]].color1} <br> €${jsonData.products[retArray[i]].price}</p>
             <div class="remove">
               <button class="remove${i} button">Remove item</button>
+              <div class="counter">
+              <div class="card__counter float-end">
+                <button class="card__btn card__btn-minus">-</button>
+              <div class="card__counter-score">1</div>
+                <button class="card__btn card__btn-plus">+</button>
+              </div>
+              </div>
             </div>
           </div>
         </div>`
@@ -38,28 +45,68 @@ function createCard(jsonData) {
       const button = document.querySelectorAll(".remove button")
       console.log(totalPrice)
       document.querySelector(".total-price").innerHTML = `Total price: €${totalPrice.toFixed(2)}`
+      if(totalPrice == 0) {
+        document.querySelector(".check-out").disabled = true;
+
+      } else if(totalPrice > 0) {
+        document.querySelector(".check-out").disabled = false;
+        document.querySelector(".check-out").addEventListener("click", function() {
+          window.location.href = "checkout.html"
+        })
+      }
       button[i].addEventListener("click", function() {
         removeArray.push(i)
         removeItem(jsonData, removeArray, i)
       })
     }
-    // const removeOne = document.querySelector(".remove0")
-    // const removeTwo = document.querySelector(".remove1")
-    // const removeThree = document.querySelector(".remove2")
-    // const removeFour = document.querySelector(".remove3")
+    const counterPlus = document.querySelectorAll(".counter .card__btn-plus")
+    const counterMinus = document.querySelectorAll(".counter .card__btn")
+    const counter = document.querySelectorAll(".counter .card__counter-score")
+    let countForReset = 0
+    for(let i = 0; i < retArray.length; i++) {
+      console.log(counterPlus.length)
+      if(i == countForReset) {
+        let counterItem = 1;
+        counterPlus[i].addEventListener("click", function() {
+          console.log(123)
+          counterItem++;
+          counter[i].innerHTML = counterItem
+          totalPrice = counterItem * jsonData.products[retArray[i]].price
+          document.querySelector(".total-price").innerHTML = `Total price: €${totalPrice.toFixed(2)}`
+          if(totalPrice == 0) {
+            document.querySelector(".check-out").disabled = true;
 
-    // removeOne.addEventListener("click", function() {
-    //   removeItem(0)
-    // })
-    // removeTwo.addEventListener("click", function() {
-    //   removeItem(1)
-    // })
-    // removeThree.addEventListener("click", function() {
-    //   removeItem(2)
-    // })
-    // removeFour.addEventListener("click", function() {
-    //   removeItem(3)
-    // })
+          } else if(totalPrice > 0) {
+            document.querySelector(".check-out").disabled = false;
+            document.querySelector(".check-out").addEventListener("click", function() {
+              window.location.href = "checkout.html"
+            })
+          }
+        })
+        counterMinus[i].addEventListener("click", function() {
+          console.log(123)
+          counterItem--;
+          if(counterItem == 0) {
+            counterItem = 0
+            removeItem(jsonData, removeArray, i)
+          }
+          counter[i].innerHTML = counterItem
+          totalPrice = counterItem * jsonData.products[retArray[i]].price
+          document.querySelector(".total-price").innerHTML = `Total price: €${totalPrice.toFixed(2)}`
+          if(totalPrice == 0) {
+            document.querySelector(".check-out").disabled = true;
+
+          } else if(totalPrice > 0) {
+            document.querySelector(".check-out").disabled = false;
+            document.querySelector(".check-out").addEventListener("click", function() {
+              window.location.href = "checkout.html"
+            })
+          }
+        })
+        countForReset++;
+      }
+      
+    }
 }
 
 function removeItem(jsonData, array, index) {
@@ -81,6 +128,15 @@ function removeItem(jsonData, array, index) {
       localStorage.setItem("cart-length", cartLength)
       updateCartItems.innerHTML = `You have ${cartLength} items in your cart`
       document.querySelector(".total-price").innerHTML = `Total price: €${totalPrice.toFixed(2)}`
+      if(totalPrice == 0) {
+        document.querySelector(".check-out").disabled = true;
+
+      } else if(totalPrice > 0) {
+        document.querySelector(".check-out").disabled = false;
+        document.querySelector(".check-out").addEventListener("click", function() {
+          window.location.href = "checkout.html"
+        })
+      }
     }
       const card = `<div class="card w-auto h-auto">
       <img class="card-img-top w-50 h-50" src="${jsonData.products[retArray[i]].img1}" alt="Filter image">
@@ -89,6 +145,13 @@ function removeItem(jsonData, array, index) {
         <p class="card-text">${jsonData.products[retArray[i]].color1} <br> €${jsonData.products[retArray[i]].price}</p>
         <div class="remove">
           <button class="remove${i} button">Remove item</button>
+          <div class="counter">
+              <div class="card__counter float-end">
+                <button class="card__btn card__btn-minus">-</button>
+              <div class="card__counter-score">1</div>
+                <button class="card__btn card__btn-plus">+</button>
+              </div>
+              </div>
         </div>
       </div>
     </div>`
@@ -101,9 +164,64 @@ function removeItem(jsonData, array, index) {
         removeArray.push(i)
         removeItem(jsonData, removeArray, i)
       })
+      const counterPlus = document.querySelectorAll(".counter .card__btn-plus")
+      const counterMinus = document.querySelectorAll(".counter .card__btn-minus")
+      const counter = document.querySelectorAll(".counter .card__counter-score")
+      let countForReset = 0
+      for(let i = 0; i < retArray.length; i++) {
+        console.log(counterMinus.length)
+        if(i == countForReset) {
+          let counterItem = 1;
+          counterPlus[i].addEventListener("click", function() {
+            console.log(123)
+            counterItem++;
+            counter[i].innerHTML = counterItem
+            totalPrice = counterItem * jsonData.products[retArray[i]].price
+            document.querySelector(".total-price").innerHTML = `Total price: €${totalPrice.toFixed(2)}`
+            
+            if(totalPrice == 0) {
+              document.querySelector(".check-out").disabled = true;
+
+            } else if(totalPrice > 0) {
+              document.querySelector(".check-out").disabled = false;
+              document.querySelector(".check-out").addEventListener("click", function() {
+                window.location.href = "checkout.html"
+              })
+            }
+          })
+          counterMinus[i].addEventListener("click", function() {
+            console.log(123)
+            counterItem--;
+            if(counterItem == 0) {
+              counterItem = 0
+              removeItem(jsonData, removeArray, i)
+            }
+            counter[i].innerHTML = counterItem
+            totalPrice = counterItem * jsonData.products[retArray[i]].price
+            document.querySelector(".total-price").innerHTML = `Total price: €${totalPrice.toFixed(2)}`
+            
+            if(totalPrice == 0) {
+              document.querySelector(".check-out").disabled = true;
+
+            } else if(totalPrice > 0) {
+              document.querySelector(".check-out").disabled = false;
+              document.querySelector(".check-out").addEventListener("click", function() {
+                window.location.href = "checkout.html"
+              })
+            }
+          })
+          countForReset++;
+        }
+        
+      }
   }
 }
 updateCartItems.innerHTML = `You have ${itemCartLocal} items in your cart`
 function navbarChange() {
   document.querySelector(".navbar-toggler-custom-icon").classList.toggle("change");
 }
+
+
+
+
+
