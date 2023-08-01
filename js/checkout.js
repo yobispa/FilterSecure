1b5b6fc2328f415ca0c9a180453e5187cc64db09
Yoandel Obispa <302631801@student.rocmondriaan.nl>
2023-08-01 20:37:21 +0200

    went back before adding counter

--- js/checkout.js
+++ js/checkout.js
@@ -1,16 +1,7 @@
 console.log("JS Loaded")
 
-const itemCartLocal = localStorage.getItem("cart-length")
-const goToCart = document.querySelector(".go-to-cart")
-goToCart.addEventListener("click", change)
-document.querySelector(".badge").textContent = itemCartLocal
-
 const navbarToggler = document.querySelector(".navbar-toggler")
 navbarToggler.addEventListener("click", navbarChange)
-
-function change() {
-    location.href = 'cart.html'
-}
 function navbarChange() {
     document.querySelector(".navbar-toggler-custom-icon").classList.toggle("change");
 }
